5be24c95590290a361e48bad280662dfd34eca22
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-06-18 20:48:23 +0200

    Fix another error

--- tests/unit/test_tracker.py
+++ tests/unit/test_tracker.py
@@ -558,7 +558,7 @@ def test_tracker_measure_transfer_time(
         for _ in range(10):
             cupy.cuda.runtime.memcpyAsync(
                 images.data.ptr,
-                host_images.ctypes.data,
+                host_images.data,
                 host_images.nbytes,
                 cupy.cuda.runtime.memcpyHostToDevice,
                 s2.ptr,
