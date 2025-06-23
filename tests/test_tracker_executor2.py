1711eda1b733ffae47c10337a358c2f9d3ccc85d
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-06-23 16:42:37 +0200

    Pass fake factories

--- tests/test_tracker_executor2.py
+++ tests/test_tracker_executor2.py
@@ -312,10 +312,12 @@ QI_TRACKER_ARGUMENTS = {
 
 
 def test_tracker_executor2():
-    tracker_factory = TrackerFactoryClassRegistry.create(
-        "qi_tracker", **QI_TRACKER_ARGUMENTS
-    )
-    camera_factory = CameraFactoryClassRegistry.create("dhyana2100")
+    # tracker_factory = TrackerFactoryClassRegistry.create(
+    #     "qi_tracker", **QI_TRACKER_ARGUMENTS
+    # )
+    # camera_factory = CameraFactoryClassRegistry.create("dhyana2100")
+    tracker_factory = None
+    camera_factory = None
 
     tracker_executor = TrackerExecutorController(
         camera_factory, tracker_factory, ROI_COORDINATES
