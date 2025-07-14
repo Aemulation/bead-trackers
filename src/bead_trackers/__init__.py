6aa95dcd98b413a681f0d172c25bb9d07f7e202d
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-07-14 14:47:19 +0200

    Don't import in init

--- src/bead_trackers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .bead_tracker.tracker import TrackerFactory
-from .tracker_base import (
-    TrackerProtocol,
-    TrackerFactoryClassRegistry,
-    TrackerFactoryProtocol,
-)
-
-__all__ = ["TrackerProtocol", "TrackerFactoryClassRegistry", "TrackerFactoryProtocol"]
